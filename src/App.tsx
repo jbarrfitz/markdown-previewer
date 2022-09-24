8b161f0df318e8313540194fc6cfb7228e63b941
jbarrfitz <78324944+jbarrfitz@users.noreply.github.com>
2022-09-24 12:00:17 -0500

    styling, autoselect md text

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
-import Markdown from "marked-react";
-import initState from "./initialState";
+import React, { useRef, useState, useEffect } from 'react';
+import Markdown from 'marked-react';
+import initState from './initialState';
+import { TextField } from '@mui/material';
+import './App.css';
 
 function App() {
-  const [mdText, setMdText] = useState(initState)
+  const [mdText, setMdText] = useState(initState);
 
-  const handleTextChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMdText(e.target.value);
-  }
+  };
+
+  const refName = useRef<HTMLTextAreaElement>(null);
+  // const refId = refName?.current?.id;
+  useEffect(() => {
+    refName?.current?.select();
+  }, []);
 
   return (
-    <div className="App">
-      <textarea id='editor' value={mdText}  onChange={handleTextChange}></textarea>
-      <div id='preview'>Preview Area</div>
-        <Markdown>{mdText}</Markdown>
+    <div>
+      <h1>Markdown Previewer</h1>
+      <div className='App'>
+        <div id='text-area'>
+          <h2>Markdown Text</h2>
+          <TextField
+            multiline
+            fullWidth
+            maxRows='20'
+            id='editor'
+            value={mdText}
+            inputRef={refName}
+            onChange={handleTextChange}
+          />
+        </div>
+        <div id='preview'>
+          <h2>Preview</h2>
+          <Markdown>{mdText}</Markdown>
+        </div>
+      </div>
     </div>
   );
 }
